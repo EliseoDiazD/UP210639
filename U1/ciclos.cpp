60cd1026e46853b269a00563f1eb837803845ce1
UP210639 <up210639@alumnos.upa.edu.mx>
2022-09-22 12:50:42 -0500

    U1

--- U1/ciclos.cpp
+++ U1/ciclos.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 int main()
 {
-    int suma, par, impar;
+    int suma = 0;
+    int par = 0;
+    int impar = 0;
     int n = 10;
     for (int i = 1; i <= n; i++)
     {
-        if ( i % 2 == 0)
-        par = par + i;
-        else 
-        impar = impar + i;
+        if (i%2==0)
+        {
+          par = par + i;   
+        }
+        else
+        {
+          impar = impar + i;
+        } 
     }
+    suma=par+impar;
     printf("el reusltado de la suma es: %d", par);
     printf("el reusltado de la suma es: %d", impar);
-    suma= par + impar;
     printf("el reusltado de la suma es: %d", suma );
     return 0;
 }
