0ab2a6c05ee6653eed3de172d4e6b440e2f648e6
UP210639 <up210639@alumnos.upa.edu.mx>
2022-09-27 13:23:48 -0500

    u1 y u2

--- U2/EvaluacionDeEdades.cpp
+++ U2/EvaluacionDeEdades.cpp
@@ -2,14 +2,13 @@
     Unidad 2
     Nombre del programa: Evaluacion de edades
     Autor: Eliseo Diaz Delgadillo
-    proposito del programa: Dependiendo la edad dada se categoriza en 4 clases "1er edad",  "2da edad", "3er edad" y Horas extras
+    proposito del programa: Dependiendo la edad dada se categoriza en 4 clases "1er edad",  "2da edad", "3er edad" y "Horas extras"
     fecha:
 */
 #include <iostream>
 #include <iomanip>
 #include <string>
 using namespace std;
-
 
 int main()
 {
@@ -18,21 +17,17 @@ int main()
     cin >> e;
     if (e >= 1 && e <= 150)
     {
-        if (e >= 1 && e <= 30)
-        {
+        if (e <= 30) {
             printf("1er edad \n");
         }
-        else if (e >= 31 && e <= 60)
-        {
+        else if (e >= 31 && e <= 60) {
             printf("2da edad \n");
         }
-        else if (e >= 61 && e <= 90)
-        {
+        else if (e >= 61 && e <= 90) {
             printf("3er edad \n");
         }
-        else if (e >= 91 && e<= 150)
-        {
-           printf("Horas Extras \n"); 
+        else {
+            printf("Horas Extras \n");
         }
     }
     else
